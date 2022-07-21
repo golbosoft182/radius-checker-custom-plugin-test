aa46a6a18bddec3b792be24fb739a143df6134ed
golbosoft182 <ekahandik@gmail.com>
2022-07-21 17:02:19 +0700

    test plugin radius checker

---
+++ assets/js/front.js
@@ -0,0 +1,19 @@
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 37.06, lng: -95.68 },
+  });
+
+  if (mapdata) {
+    const kmlLayer = new google.maps.KmlLayer({
+      url: mapdata.kml,
+      suppressInfoWindows: true,
+      map: map,
+    });
+
+    kmlLayer.addListener("click", (kmlEvent) => {
+      const text = kmlEvent.featureData.description;
+      console.log(kmlEvent.featureData)
+    });
+  }
+}
